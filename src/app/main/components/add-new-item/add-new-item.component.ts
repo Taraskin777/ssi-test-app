b6ffa45efe5186d59cb9d329d77be16b2381cd09
Taras Noga <taraskin2012@gmail.com>
2024-03-29 18:57:08 +0200

    add require message for input fields

--- src/app/main/components/add-new-item/add-new-item.component.ts
+++ src/app/main/components/add-new-item/add-new-item.component.ts
@@ -5,6 +5,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { take } from 'rxjs';
+
 @Component({
   selector: 'app-add-new-item',
   standalone: true,
@@ -14,10 +22,24 @@ import { MatButtonModule } from '@angular/material/button';
     MatSelectModule,
     MatButtonModule,
     CommonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './add-new-item.component.html',
   styleUrl: './add-new-item.component.scss',
 })
 export class AddNewItemComponent {
   color = 'black';
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+    info: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+    this.profileForm.reset();
+  }
 }
