aac1f9c974eef2e20a81ee80c78ed8bc88387484
Taras Noga <taraskin2012@gmail.com>
2024-03-29 15:51:31 +0200

    add form for adding new item

---
+++ src/app/core/services/items.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+// import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemsService {
+  constructor() {}
+
+  public getItems() {}
+
+  public addNewItem() {}
+
+  public editItem() {}
+
+  public removeItem() {}
+}
--- src/app/main/components/add-new-item/add-new-item.component.ts
+++ src/app/main/components/add-new-item/add-new-item.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-new-item',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: './add-new-item.component.html',
   styleUrl: './add-new-item.component.scss',
 })
-export class AddNewItemComponent {}
+export class AddNewItemComponent {
+  color = 'black';
+}
